ecf35572618cff8ed08a1004da5fcd1e68ee21c7
Anna-Grigencha <grigencha10@gmail.com>
2021-08-17 20:42:48 +0300

    src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function reverse(n) {
     let result = 0;
-    while (n) {}
-    {
+    while (n) {
         result = result * 10 + (n % 10);
         n = Math.floor(n / 10);
     }
