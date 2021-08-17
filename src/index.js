017ecf785c701c03102535a1e9d5fdadcb485a41
Anna-Grigencha <grigencha10@gmail.com>
2021-08-17 21:11:06 +0300

    feat: toReadle implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 module.exports = function reverse(n) {
     let result = 0;
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    if (n % 10 === 0) {
+        n = n / 10;
+    }
     while (n) {
         result = result * 10 + (n % 10);
         n = Math.floor(n / 10);
